96c3e02769e0bb39835fb1b63dbac3bdbf0cbb43
Thelittlecitizen8 <Thelittlecitizen8@gmail.com>
2020-10-11 11:09:16 +0300

    add statistics routes

--- src/Controllers/StatisticsController.ts
+++ src/Controllers/StatisticsController.ts
@@ -19,13 +19,18 @@ class StatisticsController implements Controller
 
     private initializeRoutes() {
         this.router.get(`${this.path}/rooms/bases/:eventId`, this.getCounterRoomsPerBase.bind(this));
+        this.router.get(`${this.path}/stands/bases/:eventId`, this.getCounterStandsPerBase.bind(this));
 
     }
 
     private async getCounterRoomsPerBase(req: express.Request, res: express.Response) {
-
-
-            this.db.query(`select COUNT(*) as count, Base.BaseID, Base.Name from Rooms, Base WHERE Rooms.EventID = ${req.params.eventId} AND  Rooms.BaseID = Base.BaseID GROUP BY Base.BaseID, Base.Name`)
+        this.db.query(`select COUNT(*) as count, Base.BaseID, Base.Name from Rooms, Base WHERE Rooms.EventID = ${req.params.eventId} AND  Rooms.BaseID = Base.BaseID GROUP BY Base.BaseID, Base.Name`)
+            .then(r => {
+                res.send(r[0]);
+            })
+    }
+    private async getCounterStandsPerBase(req: express.Request, res: express.Response) {
+            this.db.query(`select Count(*) as count, b.Name from Stand as s, Rooms as r, Base as b WHERE s.RoomsID = r.RoomsID AND r.EventID = ${req.params.eventId} AND r.BaseID = b.BaseID GROUP BY b.BaseID, b.Name`)
                 .then(r => {
                     res.send(r[0]);
                 })
